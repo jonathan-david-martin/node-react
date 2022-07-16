15b865b0e68c030f608e512407d90be87a83d66e
Jonathan Martin <jonathanmartinnyc@gmail.com>
2022-07-16 19:38:43 -0400

    first publish

---
+++ src/server/server.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 3001
+
+const app = express()
+
+app.get("/api",(req,res)=>{
+    res.json({message:"hello there"})
+})
+
+app.listen(PORT, ()=>{
+    console.log(`Server listening on ${PORT}`)
+})
+
+
