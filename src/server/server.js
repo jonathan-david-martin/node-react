d3e5fc87ed49c93da3ab49b8ffa53d6bba16a91a
Jonathan Martin <jonathanmartinnyc@gmail.com>
2022-07-16 19:53:18 -0400

    added express path to index.html in react build folder

--- src/server/server.js
+++ src/server/server.js
@@ -4,9 +4,16 @@ const PORT = process.env.PORT || 3001
 
 const app = express()
 
+app.use(express.static(path.resolve(__dirname, '../build')));
+
+
 app.get("/api",(req,res)=>{
     res.json({message:"hello there"})
 })
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  });
 
 app.listen(PORT, ()=>{
     console.log(`Server listening on ${PORT}`)
