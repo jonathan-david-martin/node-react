64df381454bec435c9528d7a7a51f251a34b9cca
Jonathan Martin <jonathanmartinnyc@gmail.com>
2022-07-16 20:23:24 -0400

    more updates

--- src/server/server.js
+++ src/server/server.js
@@ -1,4 +1,11 @@
 import express from 'express'
+import path from 'path'
+import {fileURLToPath} from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+
+// 👇️ "/home/john/Desktop/javascript"
+const __dirname = path.dirname(__filename);
 
 const PORT = process.env.PORT || 3001
 
