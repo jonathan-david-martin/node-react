75fe414e75396591e5d3ae454925e17bcedea41a
Jonathan Martin <jonathanmartinnyc@gmail.com>
2022-07-16 20:30:35 -0400

    update

--- src/server/server.js
+++ src/server/server.js
@@ -19,7 +19,7 @@ app.get("/api",(req,res)=>{
 })
 
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '../../build', 'index.html'));
   });
 
 app.listen(PORT, ()=>{
